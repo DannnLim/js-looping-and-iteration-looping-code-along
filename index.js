cea9033dada07af077ee1c5520e9e6e175ddf30c
DannnLim <Dannylim-@hotmail.com>
2022-06-28 11:30:46 +1000

    commit tree a61874d3522bf40396e7dd593ab597a00f941916

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan']
+
+function writeCards(names, events) {
+    const Arr = []
+    for (let i = 0; i < names.length; i++) {
+        let message = (`Thank you, ${names[i]}, for the wonderful ${events} gift!`)
+        Arr[i] = message;
+    }
+    return Arr
+}
+console.log(writeCards)
+
+// function countDown() {
+//     let countDown = 0;
+//     while (countDown < 11)
+//         console.log(countDown++)
+// }
+
+// console.log(countDown())
+
+function countDown() {
+    for (let countDown = 0; countDown < 11; countDown++) {
+        console.log(countDown)
+    }
+}
+console.log(countDown())
